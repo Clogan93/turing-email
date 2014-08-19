85bff3e715aaa4581315d8494798252303f966c6
David Gobaud <dgobaud@gmail.com>
2014-08-18 22:58:58 -0700

    fix EmailFolders controller naming

---
+++ app/controllers/email_folders_controller.rb
@@ -0,0 +1,11 @@
+class EmailFoldersController < ApplicationController
+  before_action :signed_in_user
+
+  respond_to :json, :html
+
+  def index
+    @gmail_labels = GmailLabel.where(:gmail_account => current_user.gmail_accounts.first)
+
+    respond_with @gmail_labels
+  end
+end
