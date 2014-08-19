3555f8cf73c90231d92e50fb1991718f37af0295
macgregordennis <macgregordennis@gmail.com>
2014-08-18 19:38:39 -0700

    Added an email threading prototype.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,9 @@ class StaticPagesController < ApplicationController
 
   def inbox
   end
+
+  def email_threading_prototype
+  end
   
   def api_docs
     render 'swagger_ui/_swagger_ui', :locals => {:discovery_url => '/api-docs/api-docs.json'}
