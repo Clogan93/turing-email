d7a482f7c04f6046ae37bdcd6f2d84109b1668d1
David Gobaud <dgobaud@gmail.com>
2014-08-31 14:08:51 -0700

    update emails and users requests tests

--- spec/requests/api/v1/emails_requests_spec.rb
+++ spec/requests/api/v1/emails_requests_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 describe Api::V1::EmailsController, :type => :request do
-  context 'when the user is NOT logged in' do
-    let(:email) { FactoryGirl.create(:email) }
+  let!(:email) { FactoryGirl.create(:email) }
+  let!(:email_other) { FactoryGirl.create(:email) }
 
+  context 'when the user is NOT logged in' do
     it 'should NOT show the email' do
       get "/api/v1/emails/#{email.uid}"
 
@@ -12,8 +13,6 @@ describe Api::V1::EmailsController, :type => :request do
   end
 
   context 'when the user is logged in' do
-    let(:email) { FactoryGirl.create(:email) }
-    let!(:email_other) { FactoryGirl.create(:email) }
     before { post '/api/v1/sessions', :email => email.user.email, :password => email.user.password }
 
     it 'should show the email' do
@@ -29,6 +28,27 @@ describe Api::V1::EmailsController, :type => :request do
 
     it 'should NOT show the other email' do
       get "/api/v1/emails/#{email_other.uid}"
+
+      expect(response).to have_http_status($config.http_errors[:email_not_found][:status_code])
+    end
+  end
+
+  context 'when the other user is logged in' do
+    before { post '/api/v1/sessions', :email => email_other.user.email, :password => email_other.user.password }
+
+    it 'should show the other email' do
+      get "/api/v1/emails/#{email_other.uid}"
+
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template('api/v1/emails/show')
+
+      email_rendered = JSON.parse(response.body)
+      expect(email_rendered['uid']).to eq(email_other.uid)
+      expect(email_rendered['uid']).not_to eq(email.uid)
+    end
+
+    it 'should NOT show the email' do
+      get "/api/v1/emails/#{email.uid}"
 
       expect(response).to have_http_status($config.http_errors[:email_not_found][:status_code])
     end
--- spec/requests/api/v1/users_requests_spec.rb
+++ spec/requests/api/v1/users_requests_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 describe Api::V1::UsersController, :type => :request do
+  let!(:user) { FactoryGirl.create(:user) }
+  let!(:user_other) { FactoryGirl.create(:user) }
+
   context 'when the user is logged in' do
-    let(:user) { FactoryGirl.create(:user) }
-    let!(:user_other) { FactoryGirl.create(:user) }
     before { post '/api/v1/sessions', :email => user.email, :password => user.password }
 
-    it 'should return the current user info' do
+    it 'should return the user info' do
       get '/api/v1/users/current'
 
       expect(response).to have_http_status(:ok)
@@ -18,9 +19,22 @@ describe Api::V1::UsersController, :type => :request do
     end
   end
 
-  context 'when the user is NOT logged in' do
-    let(:user) { FactoryGirl.create(:user) }
+  context 'when the other user is logged in' do
+    before { post '/api/v1/sessions', :email => user_other.email, :password => user_other.password }
 
+    it 'should return the other user info' do
+      get '/api/v1/users/current'
+
+      expect(response).to have_http_status(:ok)
+      expect(response).to render_template('api/v1/users/show')
+
+      user_rendered = JSON.parse(response.body)
+      expect(user_rendered['email']).to eq(user_other.email)
+      expect(user_rendered['email']).not_to eq(user.email)
+    end
+  end
+
+  context 'when the user is NOT logged in' do
     it 'should NOT return the current user info' do
       get '/api/v1/users/current'
 
