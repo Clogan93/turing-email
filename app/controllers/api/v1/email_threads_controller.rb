17b9ba529137086ae12571a33051d52320e0368e
David Gobaud <dgobaud@gmail.com>
2014-09-10 16:55:55 -0700

    recreate get_sorted_paginated_threads and make inbox and in_folder api calls use it

--- app/controllers/api/v1/email_threads_controller.rb
+++ app/controllers/api/v1/email_threads_controller.rb
@@ -20,45 +20,8 @@ class Api::V1::EmailThreadsController < ApiController
     if inbox_label.nil?
       @email_threads = []
     else
-      threads_per_page = 50
-
-      page = params[:page]
-      if page.blank?
-        page = 1
-      else
-        page = page.to_i
-      end
-      
-      num_rows = page * threads_per_page
-
-      sql = <<sql
-WITH RECURSIVE recent_email_threads AS (
-    (SELECT emails.email_thread_id AS email_thread_id, array[emails.email_thread_id] AS seen
-            FROM "emails" AS emails
-            INNER JOIN "email_folder_mappings" AS email_folder_mappings ON emails."id" = email_folder_mappings."email_id"
-            WHERE email_folder_mappings."email_folder_id" = #{inbox_label.id.to_i} AND email_folder_mappings."email_folder_type" = '#{inbox_label.class.to_s}'
-            ORDER BY emails."date" DESC LIMIT 1)
-
-    UNION ALL
-
-    (SELECT emails_lateral.email_thread_id AS email_thread_id, recent_email_threads.seen || emails_lateral.email_thread_id
-            FROM recent_email_threads,
-            LATERAL (SELECT emails_inner.email_thread_id
-                            FROM "emails" AS emails_inner
-                            INNER JOIN "email_folder_mappings" AS email_folder_mappings_inner ON emails_inner."id" = email_folder_mappings_inner."email_id"
-                            WHERE email_folder_mappings_inner."email_folder_id" = #{inbox_label.id.to_i} AND email_folder_mappings_inner."email_folder_type" = '#{inbox_label.class.to_s}' AND
-                                  emails_inner.email_thread_id <> ALL (recent_email_threads.seen)
-                            ORDER BY emails_inner."date" DESC LIMIT 1) AS emails_lateral
-            WHERE array_upper(recent_email_threads.seen, 1) < #{num_rows})
-)
-SELECT email_threads.*
-       FROM recent_email_threads
-       INNER JOIN "email_threads" AS email_threads ON email_threads."id" = recent_email_threads.email_thread_id
-       LIMIT #{threads_per_page} OFFSET #{(page - 1) * num_rows}
-sql
-
-      @email_threads = EmailThread.find_by_sql(sql)
-      @email_threads = EmailThread.joins(:emails).includes(:emails).where(:id => @email_threads).order('"emails"."date" DESC')
+      page = params[:page] ? params[:page].to_i : 1
+      @email_threads = inbox_label.get_sorted_paginated_threads(page: page)
     end
 
     render 'api/v1/email_threads/index'
@@ -75,17 +38,17 @@ sql
   end
 
   def in_folder
-    @email_folder = GmailLabel.find_by(:gmail_account => current_user.gmail_accounts.first,
+    email_folder = GmailLabel.find_by(:gmail_account => current_user.gmail_accounts.first,
                                        :label_id => params[:folder_id])
 
-    if @email_folder.nil?
+    if email_folder.nil?
       render :status => $config.http_errors[:email_folder_not_found][:status_code],
              :json => $config.http_errors[:email_folder_not_found][:description]
       return
     end
-    
-    email_thread_ids = @email_folder.emails.pluck(:email_thread_id)
-    @email_threads = EmailThread.get_threads_from_ids(email_thread_ids)
+
+    page = params[:page] ? params[:page].to_i : 1
+    @email_threads = email_folder.get_sorted_paginated_threads(page: page)
 
     render 'api/v1/email_threads/index'
   end
--- app/models/gmail_label.rb
+++ app/models/gmail_label.rb
@@ -21,4 +21,40 @@ class GmailLabel < ActiveRecord::Base
   def num_unread_threads
     return self.emails.where(:seen => false).pluck(:email_thread_id).uniq.count
   end
+  
+  # TODO write test
+  def get_sorted_paginated_threads(page: 1, threads_per_page: 50)
+    num_rows = page * threads_per_page
+
+    sql = <<sql
+WITH RECURSIVE recent_email_threads AS (
+    (SELECT emails.email_thread_id AS email_thread_id, array[emails.email_thread_id] AS seen
+            FROM "emails" AS emails
+            INNER JOIN "email_folder_mappings" AS email_folder_mappings ON emails."id" = email_folder_mappings."email_id"
+            WHERE email_folder_mappings."email_folder_id" = #{self.id.to_i} AND email_folder_mappings."email_folder_type" = '#{self.class.to_s}'
+            ORDER BY emails."date" DESC LIMIT 1)
+
+    UNION ALL
+
+    (SELECT emails_lateral.email_thread_id AS email_thread_id, recent_email_threads.seen || emails_lateral.email_thread_id
+            FROM recent_email_threads,
+            LATERAL (SELECT emails_inner.email_thread_id
+                            FROM "emails" AS emails_inner
+                            INNER JOIN "email_folder_mappings" AS email_folder_mappings_inner ON emails_inner."id" = email_folder_mappings_inner."email_id"
+                            WHERE email_folder_mappings_inner."email_folder_id" = #{self.id.to_i} AND email_folder_mappings_inner."email_folder_type" = '#{self.class.to_s}' AND
+                                  emails_inner.email_thread_id <> ALL (recent_email_threads.seen)
+                            ORDER BY emails_inner."date" DESC LIMIT 1) AS emails_lateral
+            WHERE array_upper(recent_email_threads.seen, 1) < #{num_rows})
+)
+SELECT email_threads.*
+       FROM recent_email_threads
+       INNER JOIN "email_threads" AS email_threads ON email_threads."id" = recent_email_threads.email_thread_id
+       LIMIT #{threads_per_page} OFFSET #{(page - 1) * num_rows}
+sql
+
+    email_threads = EmailThread.find_by_sql(sql)
+    email_threads = EmailThread.joins(:emails).includes(:emails).where(:id => email_threads).order('"emails"."date" DESC')
+    
+    return email_threads
+  end
 end
