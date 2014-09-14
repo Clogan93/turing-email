6c2716490582c68cdbd6fa62c1b8111fd9d01c19
David Gobaud <dgobaud@gmail.com>
2014-09-14 14:57:35 -0700

    pagination sql query update style tweak

--- app/models/gmail_label.rb
+++ app/models/gmail_label.rb
@@ -31,7 +31,8 @@ WITH RECURSIVE recent_email_threads AS (
     (SELECT emails.email_thread_id AS email_thread_id, array[emails.email_thread_id] AS seen
             FROM "emails" AS emails
             INNER JOIN "email_folder_mappings" AS email_folder_mappings ON emails."id" = email_folder_mappings."email_id"
-            WHERE email_folder_mappings."email_folder_id" = #{self.id.to_i} AND email_folder_mappings."email_folder_type" = '#{self.class.to_s}'
+            WHERE email_folder_mappings."email_folder_id" = #{self.id.to_i} AND
+                  email_folder_mappings."email_folder_type" = '#{self.class.to_s}'
             ORDER BY emails."date" DESC LIMIT 1)
 
     UNION ALL
@@ -41,7 +42,8 @@ WITH RECURSIVE recent_email_threads AS (
             LATERAL (SELECT emails_inner.email_thread_id
                             FROM "emails" AS emails_inner
                             INNER JOIN "email_folder_mappings" AS email_folder_mappings_inner ON emails_inner."id" = email_folder_mappings_inner."email_id"
-                            WHERE email_folder_mappings_inner."email_folder_id" = #{self.id.to_i} AND email_folder_mappings_inner."email_folder_type" = '#{self.class.to_s}' AND
+                            WHERE email_folder_mappings_inner."email_folder_id" = #{self.id.to_i} AND
+                                  email_folder_mappings_inner."email_folder_type" = '#{self.class.to_s}' AND
                                   emails_inner.email_thread_id <> ALL (recent_email_threads.seen)
                             ORDER BY emails_inner."date" DESC LIMIT 1) AS emails_lateral
             WHERE array_upper(recent_email_threads.seen, 1) < #{num_rows})
