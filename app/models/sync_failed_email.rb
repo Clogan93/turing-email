8b3cf37969652c8ed5ffc79c3c8f06c3af667886
David Gobaud <dgobaud@gmail.com>
2014-09-15 23:18:11 -0700

    better email sync exception handling with SyncFailedEmails

---
+++ app/models/sync_failed_email.rb
@@ -0,0 +1,15 @@
+class SyncFailedEmail < ActiveRecord::Base
+  belongs_to :email_account, polymorphic: true
+  
+  validates_presence_of(:email_account, :uid)
+  
+  def SyncFailedEmail.create_retry(result: nil, ex: nil)
+    retry_bock do
+      sync_failed_email = SyncFailedEmail.find_or_create_by!(:email_account => self,
+                                                             :uid => result.request.parameters['id'])
+      sync_failed_email.result = result.to_yaml if result
+      sync_failed_email.exception = ex.to_yaml if ex
+      sync_failed_email.save!
+    end
+  end
+end
