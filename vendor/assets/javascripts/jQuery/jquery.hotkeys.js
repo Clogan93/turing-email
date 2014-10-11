374ed988686c4db3a29f9b109ddce3d65fb676d4
David Gobaud <dgobaud@gmail.com>
2014-10-11 01:13:04 -0700

    ignore hotkeys in contenteditable elements

--- vendor/assets/javascripts/jQuery/jquery.hotkeys.js
+++ vendor/assets/javascripts/jQuery/jquery.hotkeys.js
@@ -139,9 +139,20 @@
 
     handleObj.handler = function(event) {
       //      Don't fire in text-accepting inputs that we didn't directly bind to
-      if (this !== event.target && (/textarea|select/i.test(event.target.nodeName) ||
-        (jQuery.hotkeys.options.filterTextInputs &&
-          jQuery.inArray(event.target.type, jQuery.hotkeys.textAcceptingInputTypes) > -1))) {
+      if (
+          this !== event.target &&
+          (
+            /textarea|select/i.test(event.target.nodeName)
+            ||
+            (
+              jQuery.hotkeys.options.filterTextInputs
+              &&
+              jQuery.inArray(event.target.type, jQuery.hotkeys.textAcceptingInputTypes) > -1
+            )
+            ||
+            $(event.target).attr("contenteditable")
+          )
+         ) {
         return;
       }
 
