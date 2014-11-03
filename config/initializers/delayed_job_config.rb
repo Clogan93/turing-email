f574ea5e116f093ecdaf544a4c5d92de38d63d11
David Gobaud <dgobaud@gmail.com>
2014-11-02 17:43:50 -0800

    revert new delay scaling

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -23,22 +23,16 @@ end
 
 module Delayed
   module DelayMail
-    def delay(options = {}, heroku_scale: true, dyno: 'worker', num_dynos: 1)
-      if heroku_scale
-        current_num_dynos = HerokuTools::HerokuTools.count_dynos(dyno)
-        HerokuTools::HerokuTools.scale_dynos(dyno, num_dynos) if current_num_dynos < num_dynos
-      end
+    def delay(options = {}, heroku_scale: true)
+      HerokuTools::HerokuTools.scale_dynos('worker', 1) if heroku_scale
 
       DelayProxy.new(PerformableMailer, self, options)
     end
   end
 
   module MessageSending
-    def delay(options = {}, heroku_scale: true, dyno: 'worker', num_dynos: 1)
-      if heroku_scale
-        current_num_dynos = HerokuTools::HerokuTools.count_dynos(dyno)
-        HerokuTools::HerokuTools.scale_dynos(dyno, num_dynos) if current_num_dynos < num_dynos
-      end
+    def delay(options = {}, heroku_scale: true)
+      HerokuTools::HerokuTools.scale_dynos('worker', 1) if heroku_scale
 
       DelayProxy.new(PerformableMethod, self, options)
     end
