e40aeb062e82eb25ebe7154232084c8ab0af4533
macgregordennis <macgregordennis@gmail.com>
2014-11-07 17:34:26 -0800

    Removed the stats method, swagger api reference and route from the app.

--- app/controllers/api/v1/apps_controller.rb
+++ app/controllers/api/v1/apps_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::AppsController < ApiController
   before_action { signed_in_user(true) }
 
-  before_action :correct_user, :except => [:test, :stats, :create, :index, :install, :uninstall]
+  before_action :correct_user, :except => [:test, :create, :index, :install, :uninstall]
   before_action :with_app, :only => [:install, :uninstall]
 
   swagger_controller :apps, 'Apps Controller'
@@ -29,72 +29,6 @@ class Api::V1::AppsController < ApiController
     param :form, :email_thread, :string, false, 'Email Thread'
 
     response :ok
-  end
-
-  def stats
-    email_thread = params[:email_thread]
-    emails = email_thread[:emails]
-
-    html = "<div><strong>Email Thread Stats</strong></div>"
-    html += "<div>Num messages: " + email_thread[:num_messages].to_s + "</div>"
-
-    #Word count
-    email_thread_word_count = 0
-    emails.each do |index, email|
-      if email["text_part"]
-        email_thread_word_count += email["text_part"].split(" ").length
-      end
-    end
-
-    html += "<div>Word count: " + email_thread_word_count.to_s + "</div>"
-
-    #Num addresses:
-    addresses = []
-    emails.each do |index, email|
-      addresses.push email["from_address"]
-      tos = email["tos"].split(",")
-      if email["ccs"]
-        ccs = email["ccs"].split(",")
-        ccs.each do |cc|
-          addresses.push cc
-        end
-      end
-      if email["bccs"]
-        bccs = email["bccs"].split(",")
-        bccs.each do |bcc|
-          addresses.push bcc
-        end
-      end
-    end
-
-    html += "<div>Num addresses: " + addresses.uniq.length.to_s + "</div>"
-    
-    #Most common word
-    words = []
-    emails.each do |index, email|
-      if email["text_part"]
-        text_part = email["text_part"].gsub(/[^0-9a-z ]/i, '')
-        words += text_part.split(" ")
-      end
-    end
-    if words.length > 0
-      most_common_word = most_common_value(words)
-      html += "<div>Most common word: " + most_common_word.to_s + "</div>"
-    end
-
-    #Duration
-    if emails.length > 1
-      log_console "EMAILSYO4"
-      first_email_date = Time.parse(emails["0"]["date"])
-      log_console first_email_date
-      last_email = emails[(emails.length - 1).to_s]
-      last_email_date = Time.parse(last_email["date"])
-      log_console last_email_date
-      num_hours = ((last_email_date - first_email_date) / 1.hour).round
-      html += "<div>Duration: " + num_hours.to_s + " hours</div>"
-    end
-
-    render :html => html.html_safe
   end
 
   swagger_api :create do
