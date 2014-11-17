505362596e951adc68c06c4578a1f51a5ca5f715
macgregordennis <macgregordennis@gmail.com>
2014-11-17 02:22:10 -0800

    Fixed issue with the compose dropdown staying green in the second skin.

--- db/schema.rb
+++ db/schema.rb
@@ -366,8 +366,6 @@ ActiveRecord::Schema.define(version: 20141117030450) do
     t.datetime "most_recent_email_date"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "unsubscribe_delayed_job_id"
-    t.boolean  "unsubscribed",               default: false
   end
 
   add_index "list_subscriptions", ["email_account_id", "list_id", "list_domain"], name: "index_list_subscriptions_on_ea_id_and_list_id_list_domain", unique: true, using: :btree
