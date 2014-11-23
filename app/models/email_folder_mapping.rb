4af464be25004b50a11fcce4b433d3885ffc3920
David Gobaud <dgobaud@gmail.com>
2014-11-23 02:03:41 -0800

    store email folder counts

--- app/models/email_folder_mapping.rb
+++ app/models/email_folder_mapping.rb
@@ -3,6 +3,13 @@ class EmailFolderMapping < ActiveRecord::Base
   belongs_to :email_thread
   belongs_to :email_folder, polymorphic: true
 
-
   validates_presence_of(:email_id, :email_thread_id, :email_folder_id, :email_folder_type)
+  
+  after_create {
+    self.email_folder.update_counts()
+  }
+
+  after_destroy {
+    self.email_folder.update_counts()
+  }
 end
